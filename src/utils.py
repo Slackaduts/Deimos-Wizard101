04aeca3785f68ac9716caad5435dc28e5d11b979
Ultimate314 <104117306+Ultimate314@users.noreply.github.com>
2023-04-22 18:13:58 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,8 @@ import traceback
 import requests
 
 import wizwalker.errors
-from wizwalker import Client, Keycode, XYZ, user32
+from wizwalker import Client, Keycode, XYZ, kernel32
+from wizwalker.utils import get_all_wizard_handles, override_wiz_install_location, get_pid_from_handle
 from wizwalker.extensions.scripting.utils import _maybe_get_named_window, _cycle_to_online_friends, _click_on_friend, _teleport_to_friend, _friend_list_entry
 from wizwalker.extensions.wizsprinter.wiz_navigator import toZone
 from wizwalker.memory import Window, WindowFlags
@@ -1236,3 +1237,15 @@ def assign_pet_level(destinationLevel):
 		for index, track in enumerate(wizard_city_dance_game_path):
 			if (track in pet_world_tracks):
 				wizard_city_dance_game_path[index] = selected_track
+
+def override_wiz_install_using_handle(max_size = 100):
+    """
+    This function allows you to automatically override your wiz install location, provided that wizard101 is open.
+    """
+    path = ctypes.create_unicode_buffer(max_size)
+    pid = get_pid_from_handle(get_all_wizard_handles()[0])
+    handle = kernel32.OpenProcess(0x410, 0, pid) # PROCESS_QUERY_INFORMATION and PROCESS_VM_READ
+    ctypes.windll.psapi.GetModuleFileNameExW(handle, None, ctypes.byref(path), max_size)
+    kernel32.CloseHandle(handle)
+    install_location = path.value.replace("\Bin\WizardGraphicalClient.exe", "")
+    override_wiz_install_location(install_location)
