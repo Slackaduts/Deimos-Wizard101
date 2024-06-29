62f5f15c4cd9bb2eaacaa5b63b668cd4f09d1a09
Shadowstrike13 <laffyxtaffy13@gmail.com>
2024-06-28 21:48:01 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -419,11 +419,12 @@ async def navigate_to_commons_from_ravenwood(client: Client):
 
 
 async def navigate_to_potions(client: Client):
-   # Teleport to hilda brewer
-    await asyncio.sleep(1)
-    await client.teleport(XYZ(x=-4398.70654296875, y=1016.1954345703125, z=229.00079345703125))
-    logger.debug(f'Client {client.title} - Teleported to Hilda')
-    # await client.send_key(Keycode.S, 0.1)
+    # make sure client is not loading
+    while await client.is_loading():
+      await asyncio.sleep(0.1)
+    # Teleport to hilda brewer
+    hilda = XYZ(-4398.70654296875, 1016.1954345703125, 229.00079345703125)
+    await client.teleport(hilda)
 
 
 async def buy_potions(client: Client, recall: bool = True, original_zone=None):
